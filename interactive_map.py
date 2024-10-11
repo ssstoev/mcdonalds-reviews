aea2036a6d4adbbe6f03f260328978a90df9d71d
Stefan Stoev <stoev1606@gmail.com>
2024-10-11 22:25:42 +0200

    improved the interactive map to include the rating of restaurants

--- interactive_map.py
+++ interactive_map.py
@@ -7,26 +7,37 @@ import folium
 from folium import plugins
 import matplotlib
 import matplotlib.pyplot as plt
+import branca
 
 data = pd.read_csv("McDonald_s_Reviews.csv", encoding='ISO-8859-1')
 data.dropna(inplace=True)
+
+# Convert the rating column to a numeric one
+data.loc[:, "rating (stars)"] = data["rating"].str.split(' ').map(lambda x: x[0] if len(x) > 0 else None)
+data = data.drop(["rating"], axis=1)
+data["rating (stars)"] = data["rating (stars)"].astype(int)
+
 m = folium.Map(location=[data["latitude "].mean(), data["longitude"].mean()], zoom_start=6)
 
 nr_ratings_min, nr_ratings_max = data["rating_count"].min(), data["rating_count"].max()
 
-# Normalize ratings_count to use for color mapping
-norm = plt.Normalize(data['rating_count'].min(), data['rating_count'].max())
+# Normalize ratings_count and the given stars to use for color mapping
+# norm_rating_count = plt.Normalize(data['rating_count'].min(), data['rating_count'].max())
+norm_rating_stars = plt.Normalize(data["rating (stars)"].min(), data["rating (stars)"].max())
+
 # Create a colormap
-cmap = plt.cm.get_cmap('RdYlGn')
+#cmap = plt.cm.get_cmap('RdYlGn')
+cmap = plt.colormaps["RdYlGn"]
 
 for _, row in data.iterrows():
 
-    popup_info = f''''Rating for store {row["rating"]} <br>
-    Number of ratings for store: {row["rating_count"]}
+    popup_info = f''''Rating for restaurant: {row["rating (stars)"]} <br>
+    Number of ratings for restaurant: {row["rating_count"]}
     '''
     # Normalize the rating and map to a color
-    normalized_value = norm(row['rating_count'])
-    color = cmap(normalized_value)
+    # normalized_value = norm_rating_count(row['rating_count'])
+    normalized_rating_stars = norm_rating_stars(row["rating (stars)"])
+    color = cmap(normalized_rating_stars)
     
     # Convert the RGBA color to hex for folium
     hex_color = matplotlib.colors.rgb2hex(color)
@@ -41,6 +52,11 @@ for _, row in data.iterrows():
         popup=folium.Popup(popup_info, max_width=300)
     ).add_to(m)
 
+# Add a color scale (legend) from red to green
+colormap = branca.colormap.LinearColormap(colors=['red', 'yellow', 'green'], vmin=0, vmax=5)
+colormap.caption = "Rating (Stars)"
+colormap.add_to(m)
+
 plugins.MiniMap().add_to(m)
 
-m.save("mcdonalds_review.html")
+m.save("mc_donalds_interactive_map.html")
